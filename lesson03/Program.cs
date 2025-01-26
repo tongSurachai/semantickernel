ea73e4885b8e440f23117517d0ca6f2bcf9c1d28
Ameer Deen <writeameer@gmail.com>
2025-01-26 22:50:08 +0400

    Added docs

--- lesson03/Program.cs
+++ lesson03/Program.cs
@@ -2,21 +2,30 @@
 using Microsoft.SemanticKernel;
 using OllamaSharp;
 
+// Setup kernel to use Ollama chat completion
 var builder = Kernel.CreateBuilder().AddOllamaChatCompletion(
      new OllamaApiClient("http://localhost:11434","llama3.2:latest")
 );
 
+// Add MangoPlugin to the kernel
 builder.Plugins.AddFromType<MangoPlugin>();
+
+// Build the kernel
 var kernel = builder.Build();
 
+// Configure the kernel to automatically choose the function
 var settings = new PromptExecutionSettings() {
     FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() 
 };
 
+// Send a prompt to the kernel
 var response = await kernel.InvokePromptAsync("How much for .5 kg of mangoes",new (settings));
 
+// Print the response
 Console.WriteLine(response);
 
+
+// Define a MangoPlugin class with a GetMangoPrice function
 public class MangoPlugin
 {
     [KernelFunction("GetMangoPrice")]
